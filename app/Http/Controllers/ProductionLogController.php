0f04857cc410567b823fdd8b862917522ef8815c
devlaihub <dev.lautanairindonesia@gmail.com>
2026-02-21 16:15:09 +0700

    update nullable sg

--- app/Http/Controllers/ProductionLogController.php
+++ app/Http/Controllers/ProductionLogController.php
@@ -25,7 +25,7 @@ class ProductionLogController extends Controller
             'product_type' => 'required|in:PACA,PACS,PACV,PACA_EXPORT,ACH,SCW',
             'total_good_product' => 'required|integer',
             'total_defect' => 'required|integer',
-            'sg' => 'nullable',
+            // 'sg' => 'nullable',
         ]);
 
         try {
@@ -39,7 +39,7 @@ class ProductionLogController extends Controller
                 'product_type' => $request->product_type,
                 'good_product' => $request->total_good_product,
                 'total_defect' => $request->total_defect,
-                'sg' => $sg,
+                // 'sg' => $sg,
                 'user_name' => Auth::user()->name,
             ]);
 
@@ -75,21 +75,21 @@ class ProductionLogController extends Controller
             'product_type' => 'required|in:PACA,PACS,PACV,PACA_EXPORT,ACH,SCW',
             'total_good_product' => 'required|integer',
             'total_defect' => 'required|integer',
-            'sg' => 'required',
+            // 'sg' => 'required',
         ]);
 
         try {
 
             // Normalisasi
-            $sg = str_replace(',', '.', $request->sg);
-            $sg = floatval($sg);
+            // $sg = str_replace(',', '.', $request->sg);
+            // $sg = floatval($sg);
 
             $productionLog = ProductionLog::findOrFail($id);
             $productionLog->date = $request->date;
             $productionLog->product_type = $request->product_type;
             $productionLog->good_product = $request->total_good_product;
             $productionLog->total_defect = $request->total_defect;
-            $productionLog->sg = $sg;
+            // $productionLog->sg = $sg;
             $productionLog->save();
 
             return response()->json(['status' => 'success', 'message' => 'Data updated successfully!']);
